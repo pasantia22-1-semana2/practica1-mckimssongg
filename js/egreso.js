041096ed69065a201bd59b00ba3ea28378620a39
mckimssongg <86619053+mckimssongg@users.noreply.github.com>
2022-03-21 11:51:08 -0600

    add

---
+++ js/egreso.js
@@ -0,0 +1,18 @@
+class Egreso extends Dato{
+    static contadorEgreso = 0;
+    constructor(descripcion, valor){
+        super(descripcion, valor)
+        this._id = ++Egreso.contador;
+    }
+    get idEgreso() {
+        return this._id
+    }
+    toString(){
+        return `
+        <div class="tabla__egresos__cuenta cuentaEstilo">
+            <div class="egresos__cuenta__descripcion">${this._descripcion}</div>
+            <div class="egresos__cuenta__valor">${this._valor}</div> 
+        </div>
+        <hr>`
+    }
+}
---
+++ js/ingreso.js
@@ -0,0 +1,18 @@
+class ingreso extends Dato{
+    static contadoringreso = 0;
+    constructor(descripcion, valor){
+        super(descripcion, valor)
+        this._id = ++ingreso.contador;
+    }
+    get idingreso() {
+        return this._id
+    }
+    toString(){
+        return `
+        <div class="tabla__ingreso__cuenta cuentaEstilo">
+        <div class="ingresos__cuenta__descripcion">${this._descripcion}</div>
+        <div class="ingresos__cuenta__valor">$ ${this._valor}</div>
+        </div>
+        <hr>`
+    }
+}s
